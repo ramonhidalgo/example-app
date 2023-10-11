c8f938e97716fdf212dd4b82bb1efd8f5672d83e
ramon <ramon@uma.es>
2023-10-11 11:18:01 +0200

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('ajaxproducts',App\Http\Controllers\ProductAjaxController::class);
